01c1c165a95621ce56fb08f43a7df64a151e941b
yhan <hanyi8110@gmail.com>
2022-02-10 22:53:45 +0100

    init

---
+++ Migrations/20220210213047_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFGetStarted.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    VWAP = table.Column<double>(type: "double precision", nullable: false),
+                    ParentOrderId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Orders_ParentOrderId",
+                        column: x => x.ParentOrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ParentOrderId",
+                table: "Orders",
+                column: "ParentOrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+using (var db = new OrderContext())
+{
+    // Create
+    Console.WriteLine("Inserting a new Order");
+    db.Add(new Order
+    {
+        Id = "helloOrder",
+        ParentOrder = null,
+        Type = "SOR",
+        VWAP = 42D
+    });
+    db.SaveChanges();
+
+    // Read
+    Console.WriteLine("Querying for a blog");
+    var order = db.Orders
+        .OrderBy(b => b.Id)
+        .First();
+
+    // Update
+    Console.WriteLine("Create a parent order. And assign the create order to an existing order");
+    var parent = new Order
+    {
+        Id = "parentOrder",
+        ParentOrder = null,
+        Type = "Parent",
+        VWAP = 42D*023
+    };
+    order.Type = "ClientOrder";
+    db.Add(parent);
+    db.SaveChanges();
+
+    // Delete
+    Console.WriteLine("Delete the order");
+    db.Remove(order);
+    db.SaveChanges();
+}
+
+public class Order
+{
+    public string Id { get; set; }
+    public string Type { get; set; }
+
+    public double VWAP { get; set; }
+
+    public Order ParentOrder { get; set; }
+}
+
+public class OrderContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    
+    public OrderContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseNpgsql("Server=127.0.0.1;Port=5432;Database=dbEfCore;User Id=postgres;Password=postgres;CommandTimeout=20;SearchPath=hello;");
+}
